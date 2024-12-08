a9306ca325897039121c978172cd38ce8db3c220
Walisson José <85714808+walicard56@users.noreply.github.com>
2024-12-08 12:49:24 -0300

    Update settings.py

--- RevShare/settings.py
+++ RevShare/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-yec%4sl0o9r#pk8v59h_s0rd5-j8k+f(h=l%(x%129woaxgw&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tigrinho-71cc7b947dab.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['tigrinhocoin.online', 'localhost', '127.0.0.1']
 
 
 
